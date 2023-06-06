3529e19365cba1705cbae97bb5fa126b8b7b92c4
yonding <uwrgoy7584@khu.ac.kr>
2023-06-06 16:59:03 +0900

    Add comments

--- src/main/java/org/example/client/ReqRepBasicClient.java
+++ src/main/java/org/example/client/ReqRepBasicClient.java
@@ -13,11 +13,13 @@ public class ReqRepBasicClient {
             ZMQ.Socket socket = context.createSocket(SocketType.REQ);
             socket.connect("tcp://localhost:5555");
 
+            // Do 10 requests, waiting each time for a response
             for (int requestNbr = 0; requestNbr != 10; requestNbr++) {
                 String request = "Hello";
                 System.out.println("Sending Request " + requestNbr);
                 socket.send(request.getBytes(ZMQ.CHARSET), 0);
 
+                // Get the reply
                 byte[] reply = socket.recv(0);
                 System.out.println(
                         "Received reply "+ requestNbr+" [ " + new String(reply, ZMQ.CHARSET) + " ]"
